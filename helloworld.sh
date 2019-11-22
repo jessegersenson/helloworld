d92ce6eb5a114695f0e6539d89f50842cae7be4b
Jesse Gersenson <jessegersenson@users.noreply.github.com>
2019-11-22 11:16:27 +0100

    Update helloworld.sh

--- helloworld.sh
+++ helloworld.sh
@@ -1,9 +1,3 @@
-console.log('hi');
-const os = require('os')
-const hostname = os.hostname()
-
-logger.info('test log message')
-await run(`whoami`)
-await run(`ls -altr`)
-await run(`~/./chessmates-deploy.sh -e prod`)
+await run(`./test.sh`)
+await run(`.././chessmates-deploy.sh -e prod -p build`)
 
